7536c6337cac24f182d2b127f215557f42476f43
Nathan Fletcher <nathan.fletcher@gmail.com>
2021-09-10 11:56:20 -0700

    Show result in the UI

--- src/exchange-service.js
+++ src/exchange-service.js
@@ -1,10 +1,7 @@
 export default class ExchangeService {
-  // static rates = [];
   static convert() {
     return fetch(
       `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`
-      //changing USD to UD makes a 404
-
     )
       .then((response) => {
         if (!response.ok) {
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
 import ExchangeService from "./exchange-service";
-
+import codes from "./codes";
 /*
 Todos
 field to enter amount - form 
@@ -12,25 +12,24 @@ README - instructions, code styling
 */
 $(document).ready(function () {
   const amountInput = $("#amount-input");
+  const rates = {};
+  console.log(codes);
 
   $("#amount-form").submit(function (e) {
     e.preventDefault();
-    const amount = parseFloat(amountInput.val());
+    const amount = parseFloat(amountInput.val()) || 1;
     console.log(amount);
 
     ExchangeService.convert().then(
       (data) => {
-        if (!data.ok) {
-          console.log(
-            `The following error was encountered when making this request: ${data.status} `
-          );
-        } else {
-          console.log(data);
-        }
-        console.log("data", data);
+        rates[data.base_code] = data.conversion_rates;
+        $("#converted-display").text(`${amount} USD is equivalent to ${(amount * rates["USD"]["KRW"]).toFixed(2)} ${codes["KRW"]}`);
       },
       (response) => {
-        console.dir("response", response);
+        console.dir(
+          "response",
+          `The following error was encountered when making this request: ${response.status} `
+        );
       }
     );
   });

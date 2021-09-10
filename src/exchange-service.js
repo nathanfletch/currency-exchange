4d51e2f935eece804ea197a93a6a851e023bf936
Nathan Fletcher <nathan.fletcher@gmail.com>
2021-09-10 11:06:19 -0700

    Add basic HTML for amount input

--- src/exchange-service.js
+++ src/exchange-service.js
@@ -1,12 +1,15 @@
 export default class ExchangeService {
+  // static rates = [];
   static convert() {
     return fetch(
-      `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`
+      `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USS`
+      //changing USD to UD makes a 404
+
     )
       .then((response) => {
         if (!response.ok) {
           console.log(response);
-          throw Error(response.statusText);
+          throw Error(response);
         }
         return response.json();
       })
--- src/index.js
+++ src/index.js
@@ -4,10 +4,26 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
 import ExchangeService from "./exchange-service";
 
-$("#convert-button").click(function () {
+/*
+Todos
+field to enter amount - form 
+error handling/display
+README - instructions, code styling
+*/
+// const rates = {};
+const amountInput = $('#amount-input');
+
+$("#convert-button").submit(function (e) {
+  e.preventDefault();
+  const amount = parseFloat(amountInput.val());
+  console.log(amount);
+  
   ExchangeService.convert().then((data) => {
+    if(!data.ok) {
+      console.log("post error to dom")
+    }
     console.log("data", data);
   }, (response) => {
-    console.log("response", response)
+    console.dir("response", response)
   });
 });

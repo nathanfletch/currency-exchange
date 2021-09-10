d79ade12d0d9eb1d254061c4a7504e1e2e5141d1
Nathan Fletcher <nathan.fletcher@gmail.com>
2021-09-10 09:37:33 -0700

    Add ExchangeService class to call API

---
+++ src/exchange-service.js
@@ -0,0 +1,18 @@
+export default class ExchangeService {
+  static convert() {
+    return fetch(
+      `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`
+    )
+      .then((response) => {
+        if (!response.ok) {
+          console.log(response);
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
+      .catch((error) => {
+        console.log(error);
+        return error;
+      });
+  }
+}
--- src/index.js
+++ src/index.js
@@ -2,3 +2,12 @@ import $ from "jquery";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
+import ExchangeService from "./exchange-service";
+
+$("#convert-button").click(function () {
+  ExchangeService.convert().then((data) => {
+    console.log("data", data);
+  }, (response) => {
+    console.log("response", response)
+  });
+});
